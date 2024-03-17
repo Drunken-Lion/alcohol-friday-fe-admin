f01e3da5edbb9e548c4c5e5d1fa496497a89fd09
DASEUL <77530419+daseul-jang@users.noreply.github.com>
2024-03-17 22:15:47 +0900

    [af-284] feat: 카테고리 대분류 전체 조회 api 연결 (#8)
    1. /_service/questions.ts : getQuestions 메서드의 페이지네이션 값을 필수로
    받지 않도록 변경
    
    2. /_service/restaurants.ts : getRestaurants 메서드에 페이지네이션 값을
       전달하도록 변경

--- app/_service/questions.ts
+++ app/_service/questions.ts
@@ -5,7 +5,7 @@ const QUESTIONS_URL = '/questions';
 /**
  * /v1/admin/questions 전체 문의사항 조회
  */
-export const getQuestions = async (page: number, size: number) => {
+export const getQuestions = async (page?: number, size?: number) => {
   console.log('QuestionsService - getQuestions');
 
   const res = await axios.get(QUESTIONS_URL, { params: { page, size } });
--- app/_service/restaurants.ts
+++ app/_service/restaurants.ts
@@ -3,9 +3,9 @@ import instance from './axios';
 /**
  * /v1/admin/restaurants - 전체 매장 조회
  */
-export const getRestaurants = async () => {
+export const getRestaurants = async (page?: number, size?: number) => {
   const url = '/v1/admin/restaurants';
-  const res = await instance.get(url);
+  const res = await instance.get(url, { params: { page, size } });
 
   return res.data;
 };
