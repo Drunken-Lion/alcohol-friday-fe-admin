4c617595eb2667cd7f0ee68725f04ddac919c910
DASEUL <77530419+daseul-jang@users.noreply.github.com>
2024-03-17 21:16:09 +0900

    Feat/af 280 admin get member (#3)
    * [af-280] feat: 회원 단건 조회 api 연결
    
    * [af-280] comment: members page 주석 추가

--- app/(route)/members/page.tsx
+++ app/(route)/members/page.tsx
@@ -2,13 +2,14 @@ import Members from '@/app/_components/Members';
 import { getMembers } from '@/app/_service/members';
 
 export default async function MembersPage() {
-  // 데이터 확인용
+  // 데이터 확인용 (vscode 터미널에 로그 출력)
   const res = await getMembers();
   console.log(res.data);
 
   return (
     <div>
       MembersPage
+      {/* 컴포넌트에서 데이터 확인시, 브라우저 콘솔에 로그 출력 */}
       {/* <Members /> */}
     </div>
   );
